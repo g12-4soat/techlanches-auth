4cbabbe77b38c9bf989c07b32df73594010927b4
Deamorim <deni.barbosa.amori@gmail.com>
2024-06-24 11:28:56 -0300

    Testes previos para o endpoint de pagamento.

---
+++ TechLanchesLambdaTest/UnitTests/Services/PagamentoServiceTest.cs
@@ -0,0 +1,87 @@
+﻿using NSubstitute;
+using System.Text;
+using TechLanchesLambda.Utils;
+using TechLanchesLambda.Service;
+using System.Net;
+using TechLanchesLambdaTest.UnitTests.Fixtures;
+
+namespace TechLanchesLambdaTest.UnitTests.Services
+{
+    [Trait("Services", "Pagamento")]
+    public class PagamentoServiceTest : IClassFixture<PagamentoServiceFixture>
+    {
+        private readonly PagamentoServiceFixture _pagamentoServiceFixture;
+        //private readonly IHttpClientFactory _httpClient;
+
+        public PagamentoServiceTest(PagamentoServiceFixture pagamentoServiceFixture)
+        {
+            //_httpClient = Substitute.For<IHttpClientFactory>().CreateClient(Constants.PAGAMENTOS);
+            _pagamentoServiceFixture = pagamentoServiceFixture;
+        }
+
+        [Fact(DisplayName = "Inativar dados de pagamento do usuário com sucesso")]
+        public async Task Inativar_DadosUsuarioPagamento_DeveRetornarSucesso()
+        {
+            // Arrange
+            var cpf = _pagamentoServiceFixture.GerarCpf();
+            var token = _pagamentoServiceFixture.GerarToken();
+            var pagamentoUrl = _pagamentoServiceFixture.GerarUrlPagamento();
+
+            var httpClientFactory = Substitute.For<IHttpClientFactory>();
+            var httpClient = Substitute.For<HttpClient>();
+
+            httpClientFactory.CreateClient(Constants.PAGAMENTOS).Returns(httpClient);
+
+            var httpResponseMessage = new HttpResponseMessage
+            {
+                StatusCode = HttpStatusCode.OK,
+                Content = new StringContent(string.Empty, Encoding.UTF8, "application/json")
+            };
+
+            httpClient
+                .PostAsync(Arg.Is<string>(uri => uri == pagamentoUrl), Arg.Any<StringContent>())
+                .Returns(Task.FromResult(httpResponseMessage));
+
+            var service = new PagamentoService(httpClientFactory);
+
+            // Act
+            var resultado = await service.InativarDadosUsuarioPagamento(cpf, token);
+
+            // Assert
+            Assert.Equal(Resultado.Ok(), resultado);
+        }
+
+        [Fact(DisplayName = "Inativar dados de pagamento do usuário com falha")]
+        public async Task Inativar_DadosUsuarioPagamento_DeveRetornarFalha()
+        {
+            // Arrange
+            var cpf = _pagamentoServiceFixture.GerarCpf();
+            var token = _pagamentoServiceFixture.GerarToken();
+            var pagamentoUrl = _pagamentoServiceFixture.GerarUrlPagamento();
+
+            var httpClientFactory = Substitute.For<IHttpClientFactory>();
+            var httpClient = Substitute.For<HttpClient>();
+
+            httpClientFactory.CreateClient(Constants.PAGAMENTOS).Returns(httpClient);
+
+            var httpResponseMessage = new HttpResponseMessage
+            {
+                StatusCode = HttpStatusCode.InternalServerError,
+                Content = new StringContent(string.Empty, Encoding.UTF8, "application/json")
+            };
+
+            httpClient
+                .PostAsync(Arg.Is<string>(uri => uri == pagamentoUrl), Arg.Any<StringContent>())
+                .Returns(Task.FromResult(httpResponseMessage));
+
+            var service = new PagamentoService(httpClientFactory);
+
+            // Act
+            var resultado = await service.InativarDadosUsuarioPagamento(cpf, token);
+
+            // Assert
+            Assert.False(resultado.Sucesso);
+            Assert.Contains(_pagamentoServiceFixture.ObterMensagemFalha("inativar_usuario"), resultado.Notificacoes.FirstOrDefault().Mensagem);
+        }
+    }
+}
--- src/Serverless/LambdaAuth/Service/PagamentoService.cs
+++ src/Serverless/LambdaAuth/Service/PagamentoService.cs
@@ -1,4 +1,6 @@
-﻿using TechLanchesLambda.Utils;
+﻿using System.Net.Http.Headers;
+using System.Text;
+using TechLanchesLambda.Utils;
 
 namespace TechLanchesLambda.Service
 {
@@ -16,17 +18,17 @@ namespace TechLanchesLambda.Service
         {
             try
             {
-                //_httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
-                //_httpClient.BaseAddress = new Uri($"http://localhost:5050/");
+                _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+                _httpClient.BaseAddress = new Uri($"http://localhost:5050/");
 
-                //var content = new StringContent(string.Empty, Encoding.UTF8, "application/json");
+                var content = new StringContent(string.Empty, Encoding.UTF8, "application/json");
 
-                //var response = await _httpClient.PostAsync($"api/pagamentos/inativar/{cpf}", content);
+                var response = await _httpClient.PostAsync($"api/pagamentos/inativar/{cpf}", content);
 
-                //if (response.IsSuccessStatusCode == false)
-                //    return Resultado.Falha($"Erro durante chamada api de pedidos. Status Code:{response.StatusCode}. Response: {response}.");
+                if (response.IsSuccessStatusCode == false)
+                    return Resultado.Falha($"Erro durante chamada api de pedidos. Status Code:{response.StatusCode}. Response: {response}.");
 
-                //string resultStr = await response.Content.ReadAsStringAsync();
+                string resultStr = await response.Content.ReadAsStringAsync();
 
                 return Resultado.Ok();
             }
